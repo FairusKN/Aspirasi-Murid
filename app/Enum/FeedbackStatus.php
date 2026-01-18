1e7f448f0067eafc7aa8e98902a5f3dca1e574fc
Fairus KN <kamalnapidfairus@gmail.com>
2026-01-18 15:11:55 +0700

    Chore: add underscore

--- app/Enum/FeedbackStatus.php
+++ app/Enum/FeedbackStatus.php
@@ -5,7 +5,7 @@ namespace App\Enum;
 enum FeedbackStatus: string
 {
     case Waiting = "waiting";
-    case Processing = "in progress";
+    case Processing = "in_progress";
     case Complete = "completed";
     case Rejected = 'rejected';
 }
