0a34aac6bf2264179730b16d7023d9450e20db0c
Fairus KN <kamalnapidfairus@gmail.com>
2026-01-18 15:48:48 +0700

    Feat: Create admin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Enum\UserRole;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\User\CreateStudentRequest;
+use App\Http\Requests\User\CreateAdminRequest;
 
 
 class UserController extends Controller
 {
     public function createStudent(CreateStudentRequest $request)
     {
-        $data = User::create($request->validated());
-        return back()->with($data);
+        return back()->with(User::create($request->validated()));
+    }
+
+    public function createAdmin(CreateAdminRequest $request)
+    {
+        $fields = $request->validated();
+        $fields['role'] = UserRole::Admin->value;
+        return back()->with(User::create($fields));
     }
 }
---
+++ app/Http/Requests/User/CreateAdminRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Enum\UserRole;
+
+class CreateAdminRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->role === UserRole::SuperAdmin->value;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => "required|string|unique:users,username",
+            'full_name' => 'required|string',
+            'password' => 'required|string|min:8'
+        ];
+    }
+}
