1365b4c71133bf87e732265c419d285d20faae14
Fairus KN <kamalnapidfairus@gmail.com>
2026-01-22 17:05:55 +0700

    chore: change func name

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,24 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests\Category\CreateCategoryRequest;
 use App\Http\Requests\Category\UpdateCategoryRequest;
 
 class CategoryController extends Controller
 {
-    public function createCategory(CreateCategoryRequest $request)
+    public function create(CreateCategoryRequest $request)
     {
         return back()->with(Category::create($request->validated()));
     }
 
-    public function updateCategory(UpdateCategoryRequest $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
         return back()->with($category->update($request->validated));
     }
 
-    public function deleteCategory(Category $category)
+    public function destroy(Category $category)
     {
         $category->delete();
         return redirect()->route('pages.dashboard');
