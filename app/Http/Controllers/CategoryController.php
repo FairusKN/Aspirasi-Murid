34a7e4a1905ebff4fa01a615ec84955505c6afb7
Fairus KN <kamalnapidfairus@gmail.com>
2026-01-22 17:07:12 +0700

    chore: add comment

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,16 +11,19 @@ class CategoryController extends Controller
 {
     public function create(CreateCategoryRequest $request)
     {
+        // return the create data from req
         return back()->with(Category::create($request->validated()));
     }
 
     public function update(UpdateCategoryRequest $request, Category $category)
     {
+        // return the updated data from req
         return back()->with($category->update($request->validated));
     }
 
     public function destroy(Category $category)
     {
+        // delete the data then return to dashboard
         $category->delete();
         return redirect()->route('pages.dashboard');
     }
