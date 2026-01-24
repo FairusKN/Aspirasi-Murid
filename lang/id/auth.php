0e666d7d0f9629fbbfdf1d7c05375f532890305c
Fairus KN <kamalnapidfairus@gmail.com>
2026-01-24 19:13:38 +0700

    feat: new message

--- lang/id/auth.php
+++ lang/id/auth.php
@@ -3,5 +3,6 @@
 return [
     'failed' => "Kredensial salah",
     "password" => "Password yang diberikan salah",
-    "throttle" => "Terlalu banyak percobaan login. Coba lagi dalam :seconds detik."
+    "throttle" => "Terlalu banyak percobaan login. Coba lagi dalam :seconds detik.",
+    'not_active' => "Pengguna ini tidak dalam aktivasi. Kontak admin untuk me-aktivasi akun"
 ];
